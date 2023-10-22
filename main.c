b1d84c0bf447474c5b55fa72f2f3ecd6782b5c0a
WilliamWolezz <115882705+WilliamWolezz@users.noreply.github.com>
2023-10-22 14:30:06 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,72 +1,88 @@
 #include <stdio.h>
 
-int main() 
-{
+int main() {
     char jawab;
-    float absensi, tugas, uh, uts, nilaiakhir;
 
     do {
-        int jumlahBilangan = 0, i;
-        double bilangan, totalTugas = 0, totalUH = 0;
-
-        printf("\n|-----------------------------------|");
-        printf("\n| MENENTUKAN NILAI AKHIR SISWA - C  |");
-        printf("\n|-----------------------------------|");
+        int jumlahTugas = 0, jumlahUH = 0, i, j;
+        double nilaiTugas, nilaiUH, totalTugas = 0, totalUH = 0;
+        float uts, nilaiakhir;
+        printf("\n|---------------------------------------|");
+        printf("\n      MENENTUKAN NILAI AKHIR SISWA  ");
+        printf("\n|---------------------------------------|");
         printf("\n");
-        printf("\nMASUKKAN NILAI TUGAS = ");
 
-        printf("\nBerapa banyak nilai tugas yang ingin Anda masukkan? ");
-        scanf("%d", &jumlahBilangan);
+        // Meminta input jumlah tugas dengan loop do-while
+        do {
+            printf("\nMASUKKAN NILAI TUGAS = ");
+            printf("\nBerapa banyak nilai tugas yang ingin Anda masukkan? ");
+            if (scanf("%d", &jumlahTugas) != 1 || jumlahTugas <= 0) {
+                printf("Input jumlah tugas tidak valid. Harus berupa bilangan bulat positif.\n");
+                while (getchar() != '\n'); // Membersihkan buffer input
+                continue; // Kembali ke atas loop untuk mengulangi input
+            }
+        } while (jumlahTugas <= 0);
 
-        for (i = 0; i < jumlahBilangan; i++) {
+        i = 0;  // Inisialisasi i di sini
+        do {
             printf("Masukkan nilai tugas ke-%d: ", i + 1);
-            // Loop untuk memastikan input adalah float
-            while (scanf("%lf", &bilangan) != 1) {
-                printf("\nInput tidak valid. \nMasukkan nilai tugas ke-%d lagi: ", i + 1);
+            if (scanf("%lf", &nilaiTugas) == 1 && nilaiTugas > 0 && (int)nilaiTugas == nilaiTugas) {
+                totalTugas += nilaiTugas / jumlahTugas;
+                i++;
+            } else {
+                printf("Input tidak valid. Masukkan bilangan bulat pada nilai tugas ke-%d lagi.\n", i + 1);
                 while (getchar() != '\n'); // Membersihkan buffer input
             }
-            totalTugas += bilangan / jumlahBilangan;
-        }
+        } while (i < jumlahTugas);
 
-        printf("\n ");
-        printf("\nMASUKKAN NILAI UH = ");
-        printf("\nBerapa banyak nilai UH yang ingin Anda masukkan? ");
-        scanf("%d", &jumlahBilangan);
+        // Meminta input jumlah UH dengan loop do-while
+        do {
+            printf("\nMASUKKAN NILAI UH = ");
+            printf("\nBerapa banyak nilai UH yang ingin Anda masukkan? ");
+            if (scanf("%d", &jumlahUH) != 1 || jumlahUH <= 0) {
+                printf("Input jumlah UH tidak valid. Harus berupa bilangan bulat positif.\n");
+                while (getchar() != '\n'); 
+                continue;
+            }
+        } while (jumlahUH <= 0);
 
-        for (i = 0; i < jumlahBilangan; i++) {
-            printf("Masukkan nilai UH ke-%d: ", i + 1);
-            while (scanf("%lf", &bilangan) != 1) {
-                printf("\nInput tidak valid. \nMasukkan nilai UH ke-%d lagi: ", i + 1);
+        j = 0;  
+        do {
+            printf("Masukkan nilai UH ke-%d: ", j + 1);
+            if (scanf("%lf", &nilaiUH) == 1 && nilaiUH > 0 && (int)nilaiUH == nilaiUH) {
+                totalUH += nilaiUH / jumlahUH;
+                j++;
+            } else {
+                printf("Input tidak valid. Masukkan bilangan bulat pada nilai UH ke-%d lagi.\n", j + 1);
                 while (getchar() != '\n'); 
             }
-            totalUH += bilangan / jumlahBilangan;
-        }
+        } while (j < jumlahUH);
 
-        printf("\n ");
-        printf("\nMASUKKAN NILAI UTS = ");
-        while (scanf("%f", &uts) != 1) {
-            printf("\nInput tidak valid. \nMasukkan nilai UTS lagi: ");
-            while (getchar() != '\n'); 
-        }
+      // Meminta input UTS dengan loop do-while
+      do {
+          printf("MASUKKAN NILAI UTS = ");
+          if (scanf("%f", &uts) == 1 && uts > 0 && (int)uts == uts) {
+              break;
+          }
+          printf("Input tidak valid. Masukkan bilangan bulat pada nilai UTS.\n");
+          while (getchar() != '\n');
+      } while (1);
 
+
+        // Menghitung nilai akhir dan menampilkan hasil
         nilaiakhir = (totalTugas + totalUH + uts) / 3;
 
-        printf("\n ");
         printf("\nNILAI AKHIR = %.2lf\n", nilaiakhir);
 
-        if (nilaiakhir >= 80) {
+        if (nilaiakhir >= 90) {
             printf("NILAI HURUF = A\n");
-        }
-        else if (nilaiakhir >= 70) {
+        } else if (nilaiakhir >= 80) {
             printf("NILAI HURUF = B\n");
-        }
-        else if (nilaiakhir >= 60) {
+        } else if (nilaiakhir >= 70) {
             printf("NILAI HURUF = C\n");
-        }
-        else if (nilaiakhir >= 50) {
+        } else if (nilaiakhir >= 60) {
             printf("NILAI HURUF = D\n");
-        }
-        else {
+        } else {
             printf("NILAI HURUF = E\n");
         }
 
